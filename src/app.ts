c486a39068a0197c69fe80685975f10113627556
Justin Mann <jusmann@amazon.com>
2018-03-19 12:45:26 -0700

    remove webpack

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,47 @@
 import express from "express";
-import cppWASM from "./cppController.clist";
-// import sjWASM from "./sjController.clist";
+import cppWASM from "./cppController";
 import fs from "fs";
 import jsController from "./jsController";
 import fakeService from "./fakeService";
+
+const cppController = cppWASM({
+    wasmBinaryFile : "dist/cppController.wasm"
+});
 
 const port = process.env.PORT || 3000;
 const app = express();
 const NS_PER_SEC = 1e9;
 
-cppWASM.initialize({
-    wasmBinary: fs.readFileSync("dist/cppController.wasm")
-}).then(cppController => {
-// sjWASM.initialize({
-//     wasmBinary: fs.readFileSync("dist/sjController.wasm")
-// }).then(sjController => {
-    app.get("/cpp-hyperscript", (req, res) => {
-        cppController.handleRequestHyperscript(req, function(s: string) { res.send(s); }, fakeService);
-    });
-    app.get("/cpp-string", (req, res) => {
-        cppController.handleRequestString(req, function(s: string) { res.send(s); }, fakeService);
-    });
-    app.get("/cpp-stringstream", (req, res) => {
-        cppController.handleRequestStringStream(req, function(s: string) { res.send(s); }, fakeService);
-    });
-    app.get("/cpp-ping", (req, res) => {
-        cppController.handleRequestPing(req, function(s: string) { res.send(s); }, fakeService);
-    });
-    // app.get("/sj-hyperscript", (req, res) => {
-    //     sjController.handleRequestHyperscript(req, function(s: string) { res.send(s); }, fakeService);
-    // });
-    // app.get("/sj-string", (req, res) => {
-    //     sjController.handleRequestString(req, function(s: string) { res.send(s); }, fakeService);
-    // });
-    app.get("/js-hyperscript", (req, res) => {
-        jsController.handleRequestHyperscript(req, res);
-    });
-    app.get("/js-handlebars", (req, res) => {
-        jsController.handleRequestHandlebars(req, res);
-    });
-    app.get("/js-string", (req, res) => {
-        jsController.handleRequestString(req, res);
-    });
-    app.get("/js-ping", (req, res) => {
-        res.send("ok");
-    });
-    app.listen(port, () => console.log("Example app listening on port " + port));
-// });
+app.get("/cpp-hyperscript", (req, res) => {
+    cppController.handleRequestHyperscript(req, function(s: string) { res.send(s); }, fakeService);
 });
+app.get("/cpp-string", (req, res) => {
+    cppController.handleRequestString(req, function(s: string) { res.send(s); }, fakeService);
+});
+app.get("/cpp-stringstream", (req, res) => {
+    cppController.handleRequestStringStream(req, function(s: string) { res.send(s); }, fakeService);
+});
+app.get("/cpp-ping", (req, res) => {
+    cppController.handleRequestPing(req, function(s: string) { res.send(s); }, fakeService);
+});
+// app.get("/sj-hyperscript", (req, res) => {
+//     sjController.handleRequestHyperscript(req, function(s: string) { res.send(s); }, fakeService);
+// });
+// app.get("/sj-string", (req, res) => {
+//     sjController.handleRequestString(req, function(s: string) { res.send(s); }, fakeService);
+// });
+app.get("/js-hyperscript", (req, res) => {
+    jsController.handleRequestHyperscript(req, res);
+});
+app.get("/js-handlebars", (req, res) => {
+    jsController.handleRequestHandlebars(req, res);
+});
+app.get("/js-string", (req, res) => {
+    jsController.handleRequestString(req, res);
+});
+app.get("/js-ping", (req, res) => {
+    res.send("ok");
+});
+app.listen(port, () => console.log("Example app listening on port " + port));
 
 export default app;
--- src/cppController.d.ts
+++ src/cppController.d.ts
@@ -1,21 +1,21 @@
-// ./cppController.clist.d.ts
-export interface Assembly {
-    // ... add declarations of exported function here.
-}
+// ./cppController.d.ts
+// export interface Assembly {
+//     // ... add declarations of exported function here.
+// }
 
 // following lines is template code.
 
 export interface Module {
-   asm : Assembly;
+//    asm : Assembly;
    handleRequestHyperscript(req: any, res: any, service: any) : void;
    handleRequestString(req: any, res: any, service: any) : void;
    handleRequestStringStream(req: any, res: any, service: any) : void;
    handleRequestPing(req: any, res: any, service: any) : void;
 }
 
-export interface ModuleLoader {
-    initialize(userModule : any) : Promise<Module>;
-}
+// export interface ModuleLoader {
+//     initialize(userModule : any) : Promise<Module>;
+// }
 
-declare const loader : ModuleLoader;
+declare const loader : (userModule : any) => Module;
 export default loader;
